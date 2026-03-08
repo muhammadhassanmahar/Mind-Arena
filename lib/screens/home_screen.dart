9e43455daf060bd8bee36304a340272b65597675
Muhammad Hassan <immaharhasaan@gmail.com>
2026-03-08 22:11:51 +0500

    update

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/contest_provider.dart';
+import '../models/contest_model.dart';
+import '../core/constants/colors.dart';
+import '../core/widgets/loading_widget.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.microtask(() {
+      Provider.of<ContestProvider>(context, listen: false).fetchContests();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    final contestProvider = Provider.of<ContestProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Puzzle Contests"),
+        backgroundColor: AppColors.primary,
+      ),
+      body: contestProvider.isLoading
+          ? const LoadingWidget()
+          : ListView.builder(
+              itemCount: contestProvider.contests.length,
+              itemBuilder: (context, index) {
+
+                ContestModel contest =
+                    contestProvider.contests[index];
+
+                return Card(
+                  margin: const EdgeInsets.all(12),
+                  child: ListTile(
+                    title: Text("Entry Fee: ${contest.entryFee}"),
+                    subtitle: Text(
+                        "Players: ${contest.playersJoined}/${contest.maxPlayers}"),
+                    trailing: ElevatedButton(
+                      onPressed: () {
+
+                        Navigator.pushNamed(
+                          context,
+                          "/contestDetail",
+                          arguments: contest,
+                        );
+                      },
+                      child: const Text("Join"),
+                    ),
+                  ),
+                );
+              },
+            ),
+    );
+  }
+}
