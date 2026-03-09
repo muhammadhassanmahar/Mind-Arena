cb42f4b997990b220c896dd5347679d4518debd3
Muhammad Hassan <immaharhasaan@gmail.com>
2026-03-09 13:26:01 +0500

    update

--- lib/screens/puzzle_screen.dart
+++ lib/screens/puzzle_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:math';
 import 'package:flutter/material.dart';
 import '../models/contest_model.dart';
@@ -11,18 +12,60 @@ class PuzzleScreen extends StatefulWidget {
 
 class _PuzzleScreenState extends State<PuzzleScreen> {
   final int gridSize = 3;
+
   late List<int> tiles;
+
   bool isCompleted = false;
+
+  int remainingSeconds = 1800; // 30 minutes
+  Timer? timer;
 
   @override
   void initState() {
     super.initState();
     _generatePuzzle();
+    _startTimer();
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
   }
 
   void _generatePuzzle() {
     tiles = List.generate(gridSize * gridSize, (index) => index);
     tiles.shuffle(Random());
+  }
+
+  void _startTimer() {
+    timer = Timer.periodic(const Duration(seconds: 1), (t) {
+      if (remainingSeconds > 0) {
+        setState(() {
+          remainingSeconds--;
+        });
+      } else {
+        timer?.cancel();
+        _timeUp();
+      }
+    });
+  }
+
+  void _timeUp() {
+    if (!isCompleted) {
+      final contest =
+          ModalRoute.of(context)!.settings.arguments as ContestModel;
+
+      Navigator.pushNamed(
+        context,
+        "/result",
+        arguments: {
+          "contest": contest,
+          "failed": true,
+          "time": 1800
+        },
+      );
+    }
   }
 
   bool _isSolved() {
@@ -41,17 +84,33 @@ class _PuzzleScreenState extends State<PuzzleScreen> {
 
     if (_isSolved() && !isCompleted) {
       isCompleted = true;
+
+      timer?.cancel();
+
       final contest =
           ModalRoute.of(context)!.settings.arguments as ContestModel;
+
+      int solveTime = 1800 - remainingSeconds;
 
       Future.delayed(const Duration(milliseconds: 400), () {
         Navigator.pushNamed(
           context,
           "/result",
-          arguments: contest,
+          arguments: {
+            "contest": contest,
+            "failed": false,
+            "time": solveTime
+          },
         );
       });
     }
+  }
+
+  String formatTime(int seconds) {
+    int minutes = seconds ~/ 60;
+    int sec = seconds % 60;
+
+    return "${minutes.toString().padLeft(2, '0')}:${sec.toString().padLeft(2, '0')}";
   }
 
   @override
@@ -66,9 +125,28 @@ class _PuzzleScreenState extends State<PuzzleScreen> {
       ),
       body: Column(
         children: [
+          const SizedBox(height: 10),
+
+          /// TIMER
+          Container(
+            padding: const EdgeInsets.all(10),
+            margin: const EdgeInsets.symmetric(horizontal: 16),
+            decoration: BoxDecoration(
+              color: Colors.black87,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Text(
+              "Time Left: ${formatTime(remainingSeconds)}",
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+            ),
+          ),
+
           const SizedBox(height: 20),
 
-          /// Puzzle Grid
+          /// PUZZLE GRID
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(16),
@@ -87,10 +165,14 @@ class _PuzzleScreenState extends State<PuzzleScreen> {
                     builder: (context, candidateData, rejectedData) {
                       return Draggable<int>(
                         data: index,
-                        feedback: _buildTile(tiles[index], contest.puzzleImage),
-                        childWhenDragging: Container(color: Colors.grey[300]),
+                        feedback:
+                            _buildTile(tiles[index], contest.puzzleImage),
+                        childWhenDragging:
+                            Container(color: Colors.grey.shade300),
                         child: _buildTile(
-                            tiles[index], contest.puzzleImage),
+                          tiles[index],
+                          contest.puzzleImage,
+                        ),
                       );
                     },
                   );
