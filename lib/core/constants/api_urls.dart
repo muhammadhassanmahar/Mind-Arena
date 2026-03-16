a4b73724234bef51fe9fff46ee5ba8b50a1377c3
Muhammad Hassan <immaharhasaan@gmail.com>
2026-03-16 12:01:55 +0500

    update

--- lib/core/constants/api_urls.dart
+++ lib/core/constants/api_urls.dart
@@ -1,18 +1,19 @@
 class ApiUrls {
 
-  static const String baseUrl = "http://127.0.0.1:8000";
+  static const String baseUrl = "http://localhost:8000";
 
-  // ================= AUTH =================
-  static const String createUser = "$baseUrl/auth/signup";
-  static const String login = "$baseUrl/auth/login";
+  // AUTH
+  static const String createUser = "$baseUrl/user/create";
 
-  // ================= CONTEST =================
-  static const String getTodayContest = "$baseUrl/contest/today";
-  static const String getAllContests = "$baseUrl/contest";
-
-  // ================= LEADERBOARD =================
+  // CONTESTS
+  static const String getContests = "$baseUrl/contests/today";
+  static const String joinContest = "$baseUrl/contest/join";
+  static const String submitResult = "$baseUrl/contest/submit";
   static const String leaderboard = "$baseUrl/contest/leaderboard";
 
-  // ================= WALLET =================
+  // WALLET
   static const String wallet = "$baseUrl/wallet";
+  static const String deposit = "$baseUrl/deposit";
+  static const String withdraw = "$baseUrl/withdraw";
+
 }
